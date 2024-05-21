8313a8e999455864b7e1c7ebdbcc594a91aa3e3a
Aiden <aidenlh@qq.com>
2024-05-21 16:55:16 +0800

    feat: 配色修改, 新增透明度，窗口置顶，配色可通过配置文件修改

--- clock.py
+++ clock.py
@@ -5,17 +5,40 @@ import pathlib
 import tkinter as tk
 import time
 
-# from ttkbootstrap import Style
 from icon import img
 from tkinter import simpledialog
 from tkinter.messagebox import showerror
 
 file_path = os.path.join(pathlib.Path.home(), 'AppData\Local\clock_config.json')
 
+default_ui_configs = {
+    'bg_color': '#ffffff',
+    'btn_color': '#ffffff',
+    'font_color': '#000000',
+    'top_most': True,
+    'alpha': 0.8
+}
+
+config_info = {}
+if os.path.exists(file_path):
+    config_info = json.load(open(file_path))
+    if not ('bg_color' in config_info['ui_configs']
+            and 'btn_color' in config_info['ui_configs']
+            and 'font_color' in config_info['ui_configs']
+            and 'top_most' in config_info['ui_configs']
+            and 'alpha' in config_info['ui_configs']):
+
+        config_info['ui_configs'] = default_ui_configs
+else:
+    time_h, time_m, time_s = 600, 0, 0
+    total_seconds = int(time_h) * 3600 + int(time_m) * 60 + int(time_s)
+    config_info = {
+        'ui_configs': default_ui_configs,
+        "time_left": total_seconds,
+        "log": []
+    }
+
 root = tk.Tk()
-# 设置主题 ['solar', 'minty', 'litera', 'pulse', 'lumen', 'yeti', 'superhero']
-# style = Style(theme='solar')
-# root = style.master
 
 # 设置图标
 with open("jMWFRdx2iLDfvc0crBee_tmp.ico", "wb+") as tmp:
@@ -24,21 +47,18 @@ root.iconbitmap("jMWFRdx2iLDfvc0crBee_tmp.ico")
 os.remove("jMWFRdx2iLDfvc0crBee_tmp.ico")
 
 root.geometry("300x170")  # 设置客户端大小
-root.resizable(0, 0)  # 设置客户端大小不可变
+root.resizable(False, False)  # 设置客户端大小不可变
 root.title("Super Long Countdown")  # 设置客户端标题
-main_frame = tk.Frame(root)
-main_frame.pack(anchor='center')
+root.configure(bg=config_info['ui_configs']['bg_color'])
 
-time_info = {}
-if os.path.exists(file_path):
-    time_info = json.load(open(file_path))
-else:
-    time_h, time_m, time_s = 299, 59, 59
-    total_seconds = int(time_h) * 3600 + int(time_m) * 60 + int(time_s)
-    time_info = {
-        "time_left": total_seconds,
-        "log": []
-    }
+# 设置标题栏颜色
+root.wm_attributes("-topmost", config_info['ui_configs']['top_most'])  # 窗口置顶
+# root.wm_attributes("-transparentcolor", bg_color)  # 透明的颜色
+root.wm_attributes("-alpha", config_info['ui_configs']['alpha'])  # 透明度
+
+# 居中的布局用的主框架
+main_frame = tk.Frame(root, bg=config_info['ui_configs']['bg_color'])
+main_frame.pack(anchor='center')
 
 
 # 更新时间显示
@@ -55,8 +75,8 @@ def update_label(label_time_left):
 
 
 # 时间显示Label
-time_label = tk.Label(main_frame, font=('calibri', 40, 'bold'), pady=20, foreground='#FF7F00')
-update_label(time_info["time_left"])
+time_label = tk.Label(main_frame, font=('calibri', 40, 'bold'), pady=20, fg=config_info['ui_configs']['font_color'], bg=config_info['ui_configs']['bg_color'])
+update_label(config_info["time_left"])
 time_label.grid(column=0, row=0, columnspan=3)
 
 
@@ -64,7 +84,7 @@ time_label.grid(column=0, row=0, columnspan=3)
 def time_counter():
     global counter_timer
     global current_time_left
-    current_time_left = time_info["time_left"] - int(time.time() - begin_time)
+    current_time_left = config_info["time_left"] - int(time.time() - begin_time)
     update_label(current_time_left)
     counter_timer = time_label.after(100, time_counter)  # 1000ms后再次调用time()函数，即1s后刷新显示
 
@@ -106,8 +126,8 @@ def func_restart():
             try:
                 time_h, time_m, time_s = input_time.split(":")
                 if 60 > int(time_m) >= 0 and 60 > int(time_s) >= 0:
-                    time_info["time_left"] = int(time_h) * 3600 + int(time_m) * 60 + int(time_s)
-                    update_label(time_info["time_left"])
+                    config_info["time_left"] = int(time_h) * 3600 + int(time_m) * 60 + int(time_s)
+                    update_label(config_info["time_left"])
                     break
                 else:
                     showerror('错误', '输入值不符合规则，请重新输入')
@@ -117,16 +137,18 @@ def func_restart():
 
 # 写记录
 def func_record():
-    time_info["log"].append([begin_time, time.time(), current_time_left])
-    time_info["time_left"] = current_time_left
+    config_info["log"].append([begin_time, time.time(), current_time_left])
+    config_info["time_left"] = current_time_left
     with open(file_path, "w") as f:
-        f.write(json.dumps(time_info))
+        f.write(json.dumps(config_info))
     print("进度已保存")
 
+font_color = config_info['ui_configs']['font_color']
+btn_color = config_info['ui_configs']['btn_color']
 
-btn1 = tk.Button(main_frame, text="开始", padx=8, pady=5, command=func_begin)
-btn2 = tk.Button(main_frame, text="暂停", padx=8, pady=5, command=func_pause)
-btn3 = tk.Button(main_frame, text="重置", padx=8, pady=5, command=func_restart)
+btn1 = tk.Button(main_frame, text="开始", padx=8, pady=5, command=func_begin, fg=font_color, bg=btn_color)
+btn2 = tk.Button(main_frame, text="暂停", padx=8, pady=5, command=func_pause, fg=font_color, bg=btn_color)
+btn3 = tk.Button(main_frame, text="重置", padx=8, pady=5, command=func_restart, fg=font_color, bg=btn_color)
 
 btn1.grid(column=0, row=1)
 btn2.grid(column=1, row=1)
