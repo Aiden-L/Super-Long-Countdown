5f677979b15de48b68b973e13891626540463ea6
Aiden <aidenlh@qq.com>
2024-06-03 21:54:29 +0800

    fix: 更新bat启动脚本

--- clock.bat
+++ clock.bat
@@ -1,2 +1,6 @@
+@echo off
+if "%1" == "h" goto begin
+mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit
+:begin
 call C:\Users\aiden\anaconda3\Scripts\activate.bat C:\Users\aiden\anaconda3
 python clock.py
